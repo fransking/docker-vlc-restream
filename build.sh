628358db67c9d5be18a8e4c1d04974dfda5dbd2a
Frans King <frans.king@sbbsystems.com>
2020-05-31 10:59:12 +0100

    Tagging changed

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
-docker build -t fransking/vlc-restream-arm32v7 .
-docker image inspect fransking/vlc-restream-arm32v7:latest --format='{{.Size}}'
+docker build -t fransking/vlc-restream:arm32v7 .
+docker image inspect fransking/vlc-restream:arm32v7 --format='{{.Size}}'
--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ docker run -d \
     -p 8081:8081 \
     -p 8082:8082 \
     -e SOURCE=$1 \
-    --name vlc-restream fransking/vlc-restream-arm32v7:latest
+    --name vlc-restream fransking/vlc-restream:arm32v7
