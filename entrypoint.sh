f5648a358a9329fff56f1bfe31a288fdd73b4fa5
Frans King <frans.king@sbbsystems.com>
2020-05-31 12:37:03 +0100

    Made DEST configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ $1 == "cvlc" ] 
 then 
-    exec /usr/bin/cvlc $SOURCE --loop --sout '#duplicate{dst=rtp{sdp=rtsp://:8081/1},dst=standard{access=http,mux=ts,dst=:8082}}'
+    exec /usr/bin/cvlc $SOURCE --loop --sout $DEST
 fi 
 
 exec "$@"
--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ docker run -d \
     -p 8081:8081 \
     -p 8082:8082 \
     -e SOURCE=$1 \
+    -e DEST=$2 \
     --name vlc-restream fransking/vlc-restream:arm32v7
